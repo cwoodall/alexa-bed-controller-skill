03669e174d08f30ec7e485c89adb331540912381
Chris Woodall <chris.j.woodall@gmail.com>
2019-06-30 00:04:52 -0400

    Implemented mode controllers for moving the head, feet and bed up and down.

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 
 const DEVICE_ID = 'emmett_bed_controller';
 const BedController = require('./bed-controller');
+const AlexaHelpers = require('./alexa-helpers.js')
 const Logger = require('./logger.js');
 
 exports.handler = async (request, context, callback) => {
   Logger.debug(`Handler request: ${JSON.stringify(request)}`);
   Logger.debug(`Handler context: ${JSON.stringify(context)}`);
 
-  
   const response = await handleRequest(request);
   Logger.debug(`Response: ${JSON.stringify(response)}`);
   callback(null, response);
 };
 
 function handleRequest(request) {
-  let { namespace, name } = request.directive.header;
+  let { namespace, name, instance} = request.directive.header;
 
   switch (namespace) {
     case 'Alexa.Discovery':
       return handleDiscovery(request);
-
-    case 'Alexa.PowerController':
-      return handlePowerControl(request);
-
     case 'Alexa':
-      if (name === 'ReportState') {
-        return handleStateReport(request);
-      }
       return {};
-
+    case 'Alexa.ModeController':
+      switch (instance) {
+      case "Bed.Position":
+        return handleBedPosition(request);
+      case "Bed.Move.Height":
+        return handleMoveBedHeight(request);
+        case "Bed.Move.Feet":
+        return handleMoveBedFeet(request);
+        case "Bed.Move.Head":
+        return handleMoveBedHead(request);
+      }  
+      return {};
     default:
-      log('ERROR', `Unknown namespace ${namespace}`);
+      Logger.log('ERROR', `Unknown namespace ${namespace}`);
       return {};
   }
 }
@@ -40,138 +44,184 @@ function handleDiscovery(request) {
   header.name = 'Discover.Response';
 
   return {
-    event: { 
+    event: {
       header: header,
       payload: {
         endpoints: [{
-          endpointId: 'flightlight-001', 
-          manufacturerName: 'Chris Woodall',
-          friendlyName: "Flashlight",
-          description: "Flashlight",
-          displayCategories: ['SWITCH'],
+          endpointId: 'bed-control',
+          manufacturerName: 'Apricity',
+          friendlyName: "Bed",
+          description: "Bed",
+          displayCategories: ['OTHER'],
+          cookie: {},
           capabilities: [
-            {
-              type: 'AlexaInterface',
-              interface: 'Alexa',
-              version: '3'
-            },
-            {
-              interface: 'Alexa.PowerController',
-              version: '3',
-              type: 'AlexaInterface',
-              properties: {
-                supported: [{
-                  name: 'powerState'
-                }],
-                retrievable: true,
-                proactivelyReported: false
-              }
-            }
+            AlexaHelpers.modeControllerCapabilityFactory(
+              "Bed.Position",
+              ["Bed Position"],
+              [
+                {value: "BedPosition.Flat", names: ["Flat"]},
+                {value: "BedPosition.MemoA", names: ["Memo A", "Sleep"]},
+                {value: "BedPosition.MemoB", names: ["Memo B", "Hanging Out"]}
+              ]
+            ),
+            AlexaHelpers.modeControllerCapabilityFactory(
+              "Bed.Move.Height",
+              ["Move Bed Height", "Bed Height", "Bed"],
+              [
+                {value: "BedMoveHeight.Up", names: ["Raise", "Up", "Move Up"]},
+                {value: "BedMoveHeight.Stop", names: ["Stop", "Halt"]},
+                {value: "BedMoveHeight.Down", names: ["Lower", "Down", "Move Down"]}
+              ]
+            ),
+            AlexaHelpers.modeControllerCapabilityFactory(
+              "Bed.Move.Feet",
+              ["Move Bed Feet", "Bed Feet", "Feet", "Legs", "Move Bed Legs", "Move Legs", "Move Feet"],
+              [
+                {value: "BedMoveFeet.Up", names: ["Raise", "Up", "Move Up"]},
+                {value: "BedMoveFeet.Stop", names: ["Stop", "Halt"]},
+                {value: "BedMoveFeet.Down", names: ["Lower", "Down", "Move Down"]}
+              ]
+            ),
+            AlexaHelpers.modeControllerCapabilityFactory(
+              "Bed.Move.Head",
+              ["Move Bed Head", "Bed Head", "Bed Torso", "Head", "Torso", "Head Unit", "Move Head", "Move Head Unit"],
+              [
+                {value: "BedMoveHead.Up", names: ["Raise", "Up", "Move Up"]},
+                {value: "BedMoveHead.Stop", names: ["Stop", "Halt"]},
+                {value: "BedMoveHead.Down", names: ["Lower", "Down", "Move Down"]}
+              ]
+            ),
           ]
-        },
-        {
-            endpointId: 'memo-a', 
-            manufacturerName: 'Chris Woodall',
-            friendlyName: "Memo A",
-            description: "Memo",
-            displayCategories: ['SWITCH'],
-            capabilities: [
-              {
-                type: 'AlexaInterface',
-                interface: 'Alexa',
-                version: '3'
-              },
-              {
-                interface: 'Alexa.PowerController',
-                version: '3',
-                type: 'AlexaInterface',
-                properties: {
-                  supported: [{
-                    name: 'powerState'
-                  }],
-                  retrievable: true,
-                  proactivelyReported: false
-                }
-              }
-            ]
-          }
-        ]
-      } 
-    } 
-  };
-}
-
-
-async function handlePowerControl(request) {
-  Logger.debug('Triggered PowerControl');
-  const token = getParticleTokenFromRequest(request);
-
-  let stateToSet;
-  // Set correct state based on the Alexa directive
-  if (request.directive.header.name === 'TurnOn') {
-    stateToSet = 'ON';
-  } else {
-    stateToSet = 'OFF';
-  }
-
-  // Set the state on the device, also get the actual state (should be the same)
-  const success = await BedController.pressFor(DEVICE_ID, token, 10);
-  Logger.debug('return: ' + success);
-  let { header, endpoint } = request.directive;
-  header.namespace = 'Alexa';
-  header.name = 'Response';
-
-  const returnContext = {
-    properties: [ {
-      namespace: 'Alexa.PowerController',
-      name: 'powerState',
-      value: "OFF",
-      timeOfSample: (new Date()).toISOString(),
-      uncertaintyInMilliseconds: 0
-    } ]
-  };
-
-  const response = {
-    context: returnContext,
-    event: {
-      header: header,
-      endpoint: endpoint,
-      payload: {}
+        }]
+      }
     }
   };
-
-  Logger.debug(`Set State: ${JSON.stringify(response)}`);
-  return response;
 }
 
-async function handleStateReport(request) {
-  let { header, endpoint } = request.directive;
-  header.name = 'StateReport';
-
+async function handleBedPosition(request) {
+  Logger.debug('Triggered BedPosition');
   const token = getParticleTokenFromRequest(request);
-  const stateBool = await BedController.isPressing(DEVICE_ID, token);
+  const modeToSet = request.directive.payload.mode;
 
-  const returnContext = { 
-    properties: [{
-      namespace: 'Alexa.PowerController',
-      name: 'powerState',
-      value: stateBool.body.result ? 'ON' : 'OFF',
-      timeOfSample: stateBool.body.coreInfo.last_heard,
-      uncertaintyInMilliseconds: 0
-    }] 
-  };
+  let switch_id = BedController.SwitchId.Max;
+  switch (modeToSet) {
+  case 'BedPosition.Flat':
+      switch_id = BedController.SwitchId.Zero;
+      break;
+  case 'BedPosition.MemoA':
+      switch_id = BedController.SwitchId.MemoA;
+      break;
+  case 'BedPosition.MemoB':
+      switch_id = BedController.SwitchId.MemoB;
+      break;
+  }
+  
+  if (switch_id === BedController.SwitchId.Max) { 
+    return AlexaHelpers.formErrorResponse(request, "INVALID_VALUE", "Invalid Bed Position."); 
+  }
+  
+  const success = await BedController.pressFor(DEVICE_ID, token, switch_id);
+  return AlexaHelpers.formModeResponse(request, "Bed.Position", modeToSet);
+}
 
-  const response = {
-    context: returnContext,
-    event: { 
-      header: header,
-      endpoint: endpoint,
-      payload: {}
-    } 
-  };
+async function handleMoveBedHeight(request) {
+  Logger.debug('Triggered BedMoveHeight');
+  const token = getParticleTokenFromRequest(request);
+  const modeToSet = request.directive.payload.mode;
 
-  Logger.debug(`State Response: ${JSON.stringify(response)}`);
-  return response;
+  let switch_id = BedController.SwitchId.Max;
+  let stop = false;
+  switch (modeToSet) {
+  case 'BedMoveHeight.Up':
+    switch_id = BedController.SwitchId.BedUp;
+    break;
+  case 'BedMoveHeight.Stop':
+    stop = true;
+    break;
+  case 'BedMoveHeight.Down':
+    switch_id = BedController.SwitchId.BedDown;
+    break;
+  }
+  
+  if (stop) {
+    const success = await BedController.release(DEVICE_ID, token);
+  } else if (switch_id === BedController.SwitchId.Max) { 
+    return AlexaHelpers.formErrorResponse(request, "INVALID_VALUE", "Invalid Bed Position."); 
+  } else {
+    await BedController.release(DEVICE_ID, token);
+    const success = await BedController.pressFor(DEVICE_ID, token, switch_id, 30000);
+    if (!success) {
+      return AlexaHelpers.formErrorResponse(request, "INVALID_VALUE", "Invalid Bed Position."); 
+    }
+  }
+  
+  return AlexaHelpers.formModeResponse(request, "Bed.Move.Height", modeToSet);
+}
+
+async function handleMoveBedFeet(request) {
+  Logger.debug('Triggered BedMoveFeet');
+  const token = getParticleTokenFromRequest(request);
+  const modeToSet = request.directive.payload.mode;
+
+  let switch_id = BedController.SwitchId.Max;
+  let stop = false;
+  switch (modeToSet) {
+  case 'BedMoveFeet.Up':
+    switch_id = BedController.SwitchId.FeetUp;
+    break;
+  case 'BedMoveFeet.Stop':
+    stop = true;
+    break;
+  case 'BedMoveFeet.Down':
+    switch_id = BedController.SwitchId.FeetDown;
+    break;
+  }
+  
+  if (stop) {
+    const success = await BedController.release(DEVICE_ID, token);
+  } else if (switch_id === BedController.SwitchId.Max) { 
+    return AlexaHelpers.formErrorResponse(request, "INVALID_VALUE", "Invalid Bed Position."); 
+  } else {
+    const success = await BedController.pressFor(DEVICE_ID, token, switch_id, 30000);
+    if (!success) {
+      return AlexaHelpers.formErrorResponse(request, "INVALID_VALUE", "Invalid Bed Position."); 
+    }
+  }
+  
+  return AlexaHelpers.formModeResponse(request, "Bed.Move.Feet", modeToSet);
+}
+
+async function handleMoveBedHead(request) {
+  Logger.debug('Triggered BedMoveHead');
+  const token = getParticleTokenFromRequest(request);
+  const modeToSet = request.directive.payload.mode;
+
+  let switch_id = BedController.SwitchId.Max;
+  let stop = false;
+  switch (modeToSet) {
+  case 'BedMoveHead.Up':
+    switch_id = BedController.SwitchId.HeadUp;
+    break;
+  case 'BedMoveHead.Stop':
+    stop = true;
+    break;
+  case 'BedMoveHead.Down':
+    switch_id = BedController.SwitchId.HeadDown;
+    break;
+  }
+  
+  if (stop) {
+    const success = await BedController.release(DEVICE_ID, token);
+  } else if (switch_id === BedController.SwitchId.Max) { 
+    return AlexaHelpers.formErrorResponse(request, "INVALID_VALUE", "Invalid Bed Position."); 
+  } else {
+    const success = await BedController.pressFor(DEVICE_ID, token, switch_id, 30000);
+    if (!success) {
+      return AlexaHelpers.formErrorResponse(request, "INVALID_VALUE", "Invalid Bed Position."); 
+    }
+  }
+  
+  return AlexaHelpers.formModeResponse(request, "Bed.Move.Head", modeToSet);
 }
 
 function getParticleTokenFromRequest(request) {
